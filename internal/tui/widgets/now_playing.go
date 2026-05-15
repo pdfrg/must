80b981a9ea3ddb1a4c23c4fc7cc7afbf46fcd6a5
pdfrg <mdscott@tutanota.com>
2026-05-15 12:47:43 -0400

    fix: remove duplicate color utils from now_playing widget
    Color utilities consolidated into colorutils.go.

--- internal/tui/widgets/now_playing.go
+++ internal/tui/widgets/now_playing.go
@@ -13,20 +13,6 @@ import (
 	"github.com/pdfrg/must/internal/models"
 )
 
-func darkenColor(hex string, factor float64) string {
-	if hex == "default" || len(hex) != 7 || hex[0] != '#' {
-		return hex
-	}
-	var r, g, b int
-	_, _ = fmt.Sscanf(hex[1:3], "%x", &r)
-	_, _ = fmt.Sscanf(hex[3:5], "%x", &g)
-	_, _ = fmt.Sscanf(hex[5:7], "%x", &b)
-	r = int(float64(r) * (1 - factor))
-	g = int(float64(g) * (1 - factor))
-	b = int(float64(b) * (1 - factor))
-	return fmt.Sprintf("#%02x%02x%02x", r, g, b)
-}
-
 type NowPlaying struct {
 	foregroundStyle lipgloss.Style
 	accentStyle     lipgloss.Style
